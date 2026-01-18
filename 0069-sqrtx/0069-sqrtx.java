4fa6817237da65097b78cca6a3bcaa648d90a4c4
Pratik Rath <156946243+pratikrath126@users.noreply.github.com>
2026-01-18 10:43:00 +0530

    Time: 1 ms (96.47%), Space: 42.6 MB (68.7%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        int ans=1;
+        long g=(long)x;
+        while(low<=high){
+            int mid=(low+high)/2;
+            long m=(long)mid*mid;
+            if(m<=g){
+                ans=mid;
+                low=mid+1;
+            }
+            else
+                high=mid-1;
+        }
+        return ans;//or high as it is first possible ans low is first not possible ans
+    }
+}
