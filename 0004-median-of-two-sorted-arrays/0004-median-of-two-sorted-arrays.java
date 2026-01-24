62b57bae5f8b50303415fbd7675c966bc45fdafe
Pratik Rath <156946243+pratikrath126@users.noreply.github.com>
2026-01-24 16:07:45 +0530

    Time: 1 ms (99.48%), Space: 49 MB (43.2%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,49 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int [] nums3=new int[nums1.length+nums2.length];
+        
         int i=0; int mid=nums1.length;
         int j=0;  int high=nums2.length;
         int k=0;
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
+        int index1=(nums1.length+nums2.length)/2;
+        int count=0;
+        int index2=index1-1;
         while(i<mid && j<high){
             if(nums1[i]>nums2[j]){
-                nums3[k++]=nums2[j++];
+                if(count==index1) ele1=nums2[j];
+                if(count==index2) ele2=nums2[j];
+                j++;
+                count++;
             }
-            else
-                nums3[k++]=nums1[i++];
+            else{
+                if(count==index1) ele1=nums1[i];
+                if(count==index2) ele2=nums1[i];
+                i++;
+                count++;
+            }
+               
         }
-        for(;i<mid;i++)
-            nums3[k++]=nums1[i];
-        for(;j<high;j++)
-            nums3[k++]=nums2[j];
-        if(nums3.length%2==1){
-             int x=nums3.length/2;
-            return (double)nums3[x];
+        for(;i<mid;i++){
+             if(count==index1) ele1=nums1[i];
+             if(count==index2) ele2=nums1[i];
+                count++;
+        }
+            
+        for(;j<high;j++){
+            if(count==index1) ele1=nums2[j];
+            if(count==index2) ele2=nums2[j];
+                count++;
+        }
+          
+        if((nums1.length+nums2.length)%2==1){
+             
+            return (double)ele1;
         }
         
-            int x=nums3.length/2;
-            int y=x-1;
+
            
-            return (double) (nums3[x]+nums3[y])/2.0;
+            return (double) (ele1+ele2)/2.0;
         
         
     }
