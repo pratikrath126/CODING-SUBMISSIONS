137df944d8df90201ee63c331b930378b41738c5
Pratik Rath <156946243+pratikrath126@users.noreply.github.com>
2026-05-24 19:55:04 +0530

    Time: 1 ms (54.17%), Space: 43.5 MB (65.14%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        Map<Character,String> letters=new HashMap<>();
+        letters.put('2',"abc");
+        letters.put('3',"def");
+        letters.put('4',"ghi");
+        letters.put('5',"jkl");
+        letters.put('6',"mno");
+        letters.put('7',"pqrs");
+        letters.put('8',"tuv");
+        letters.put('9',"wxyz");
+        List<String> ans=new ArrayList<>();
+        findCombinations(0,digits,letters,ans,"");
+        return ans;
+    }
+    public void findCombinations(int index,String digits,Map<Character,String> letters,List<String> ans,String ds){
+        if(index==digits.length()){
+            ans.add(ds);
+            return;
+        }
+        
+        String s=letters.get(digits.charAt(index));
+        for(Character c:s.toCharArray()){
+            findCombinations(index+1,digits,letters,ans,ds+c);
+
+        }
+        
+    }
+}
